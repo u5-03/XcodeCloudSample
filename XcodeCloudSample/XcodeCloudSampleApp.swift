7105f78a2b8ee7f970d3341c86f00b70b5d54dc8
yugo.sugiyama <sugiy.u.2203@gmail.com>
2023-04-09 13:36:26 +0900

    Initial Commit

---
+++ XcodeCloudSample/XcodeCloudSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudSampleApp.swift
+//  XcodeCloudSample
+//
+//  Created by yugo.sugiyama on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
